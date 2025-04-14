ecaa0de950813f35875960255df9280b16cde77b
alans78 <alanmoralesm3@gmail.com>
2025-04-13 18:18:58 -0600

    Cambio de color en los botones de las alertas en la pagina de registro

--- js/registro.js
+++ js/registro.js
@@ -53,17 +53,44 @@ document.getElementById("togglePassword1").addEventListener("click", function ()
 
    
     if (nombre === "" || correo === "" || contrasena === "" || confirmarContrasena === "") {
-        Swal.fire("Campos vacíos", "Por favor completa todos los campos.", "warning");
+        Swal.fire({
+            icon: "warning",
+            title: "Campos vacíos",
+            text: "Por favor completa todos los campos.",
+            confirmButtonText: "OK",
+            customClass: {
+                confirmButton: 'btn-secondary',
+            },
+            buttonsStyling: false // ¡Importante para usar tus propias clases!
+        });
         return;
     }
 
     if (contrasena !== confirmarContrasena) {
-        Swal.fire("Error de contraseña", "Las contraseñas no coinciden.", "error");
+        Swal.fire({
+            icon: "error",
+            title: "Error de contraseña",
+            text: "Las contraseñas no coinciden.",
+            confirmButtonText: "OK",
+            customClass: {
+                confirmButton: 'btn-secondary',
+            },
+            buttonsStyling: false // ¡Importante para usar tus propias clases!
+        });
         return;
     }
 
     if (!terminosCheck) {
-        Swal.fire("Términos no aceptados", "Debes aceptar los términos y condiciones.", "info");
+        Swal.fire({
+            icon: "info",
+            title: "Términos no aceptados",
+            text: "Debes aceptar los términos y condiciones.",
+            confirmButtonText: "OK",
+            customClass: {
+                confirmButton: 'btn-secondary',
+            },
+            buttonsStyling: false // ¡Importante para usar tus propias clases!
+        });
         return;
     }
 
@@ -75,7 +102,11 @@ document.getElementById("togglePassword1").addEventListener("click", function ()
         icon: "success",
         title: "¡Registro exitoso!",
         text: "Tu cuenta ha sido creada correctamente.",
-        confirmButtonText: "Aceptar"
+        confirmButtonText: "Aceptar",
+        customClass: {
+            confirmButton: 'btn-secondary',
+          },
+          buttonsStyling: false // ¡Importante para usar tus propias clases!
     }).then(() => {
         // Redirigir o limpiar el formulario si se desea
         window.location.href = "index.html"; // o donde quieras redirigir
