7086bd7f84a8ee4c301021ee936e64d3c893a4e8
alans78 <alanmoralesm3@gmail.com>
2025-04-24 20:56:36 -0600

    Modificacion de las pantallas de eliminar cuenta

--- public/js/eliminarcuenta2.js
+++ public/js/eliminarcuenta2.js
@@ -1,6 +1,6 @@
-document.getElementById("togglePassword2").addEventListener("click", function () {
-    const passwordInput = document.getElementById("elim_contrasena");
-    const type = passwordInput.type === "password" ? "text" : "password";
-    passwordInput.type = type;
-    this.textContent = type === "password" ? "👁️" : "🙈";
-  });
+document.getElementById("togglePassword").addEventListener("click", function () {
+  const passwordInput = document.getElementById("contrasena");
+  const type = passwordInput.type === "password" ? "text" : "password";
+  passwordInput.type = type;
+  this.textContent = type === "password" ? "👁️" : "🙈";
+});
