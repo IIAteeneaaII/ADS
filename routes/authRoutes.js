e015279404a25cd3ce8b43528593cbafccccca02
IIAteeneaaII <124459897+IIAteeneaaII@users.noreply.github.com>
2025-04-30 00:41:39 -0600

    versión anterior

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, deleteAccount } = require('../controllers/authController');
+const { register, login } = require('../controllers/authController');
 
-const { validateRegister, validateLogin, validateDeleteAcc} = require('../middlewares/validateAuth');
+const { validateRegister, validateLogin } = require('../middlewares/validateAuth');
 
 router.post('/register', validateRegister, register);
 router.post('/login', validateLogin, login);
-router.post('/deleteAcc', validateDeleteAcc, deleteAccount);
 
 module.exports = router;
