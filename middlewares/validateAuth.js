83e9a3b611895685eef9e68b868dcfa92d64bbc8
IIAteeneaaII <124459897+IIAteeneaaII@users.noreply.github.com>
2025-04-30 00:17:45 -0600

    eliminar cuenta

--- middlewares/validateAuth.js
+++ middlewares/validateAuth.js
@@ -5,24 +5,22 @@ const domainRegex = new RegExp(`@(${allowedDomains.join('|').replace(/\./g, '\\.
 
 const validateRegister = [
   body('email')
-    .isEmail().withMessage('El correo electrónico no es válido')
-    .matches(domainRegex).withMessage(`El correo debe ser de: ${allowedDomains.join(', ')}`),
+    .isEmail().withMessage('Invalid email format')
+    .matches(domainRegex).withMessage(`Email domain must be one of: ${allowedDomains.join(', ')}`),
 
   body('password')
-    .isAlphanumeric().withMessage('La contraseña solo debe contener letras y números')
-    .isLength({ min: 8 }).withMessage('La contraseña debe tener al menos 8 caracteres')
-    .isLength({ max: 12 }).withMessage('La contraseña no debe exceder los 12 caracteres'),
+    .isAlphanumeric().withMessage('Password must contain only letters and numbers')
+    .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
+    .isLength({ max: 12 }).withMessage('Password must not be more than 12 characters long'),
 
   body('userName')
-    .notEmpty().withMessage('El nombre de usuario es obligatorio')
-    .isLength({ min: 3 }).withMessage('El nombre de usuario debe tener al menos 3 caracteres'),
+    .notEmpty().withMessage('Username is required')
+    .isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
 
   (req, res, next) => {
-    console.log('body: ', req.body)
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      const mensajes = errors.array().map(err => err.msg).join('-');
-      return res.redirect(`/Registro?error=${encodeURIComponent(mensajes)}`);
+      return res.status(400).json({ errors: errors.array() });
     }
     next();
   }
@@ -30,17 +28,31 @@ const validateRegister = [
 
 const validateLogin = [
   body('email')
-    .notEmpty().withMessage('El correo es obligatorio'),
+    .notEmpty().withMessage('Email is required'),
 
   body('password')
-    .notEmpty().withMessage('La contraseña es obligatoria'),
+    .notEmpty().withMessage('Password is required'),
 
   (req, res, next) => {
-    console.log('body: ', req.body)
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      const mensajes = errors.array().map(err => err.msg).join(' - ');
-      return res.redirect(`/?error=${encodeURIComponent(mensajes)}`);
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
+];
+
+const validateDeleteAcc = [
+  body('email')
+    .notEmpty().withMessage('Email is required'),
+
+  body('password')
+    .notEmpty().withMessage('Password is required'),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
     next();
   }
@@ -49,4 +61,5 @@ const validateLogin = [
 module.exports = {
   validateRegister,
   validateLogin,
+  validateDeleteAcc
 };
