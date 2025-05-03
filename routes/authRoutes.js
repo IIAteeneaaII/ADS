35309a03ef44f5801276a42335f9104770c11645
DiegoRE-15 <diegoespinosar15@gmail.com>
2025-05-03 12:26:30 -0600

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/authController');
+const { recoverPassword } = require('../controllers/authController');
+const { validateResetToken } = require('../controllers/authController');
+const { resetPassword } = require('../controllers/authController');
 
 const { validateRegister, validateLogin } = require('../middlewares/validateAuth');
 
 router.post('/register', validateRegister, register);
 router.post('/login', validateLogin, login);
+
+router.post('/recover-password', recoverPassword);
+router.post('/validate-reset-token', validateResetToken);
+router.post('/reset-password', resetPassword);
 
 module.exports = router;
