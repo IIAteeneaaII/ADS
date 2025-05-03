9390d11655fae4994ebc053db5031c0c90f8f82a
DiegoRE-15 <diegoespinosar15@gmail.com>
2025-05-03 12:30:25 -0600

    userRepositoryPrisma.js

--- repositories/userRepositoryPrisma.js
+++ repositories/userRepositoryPrisma.js
@@ -16,3 +16,20 @@ exports.createUser = async (user) => {
 exports.getAll = async () => {
   return await prisma.user.findMany();
 };
+
+exports.updatePassword = async (email, hashedPassword) => {
+  try {
+    // Actualiza la contraseña del usuario en la base de datos
+    const updatedUser = await prisma.user.update({
+      where: {
+        email: email,  // Busca el usuario por su correo electrónico
+      },
+      data: {
+        password: hashedPassword,  // Establece la nueva contraseña
+      },
+    });
+    return updatedUser;  // Devuelve el usuario actualizado (opcional)
+  } catch (error) {
+    throw new Error('Error updating password: ' + error.message);
+  }
+};
