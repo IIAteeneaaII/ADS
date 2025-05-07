4839b51538ce92dab2d23288c7f209a90983e7c6
TheAntoniee <rivera.martinez.cristhian.antonio@gmail.com>
2025-05-06 21:33:44 -0600

    Gestionar Hábitos

--- public/js/gestionarHabitos.js
+++ public/js/gestionarHabitos.js
@@ -1,19 +1,21 @@
 const HABITOS = {
     movimiento: [
-        { img: '../img/gestorhabitos/estiramiento.png', nombre: 'Estiramientos matutinos', link: '/estiramientos' },
-        { img: '../img/gestorhabitos/correr.png', nombre: 'Correr', link: 'movimiento2.html' },
-        { img: '../img/gestorhabitos/bici.png', nombre: 'Andar en bicicleta', link: 'movimiento3.html' },
-        { img: '../img/gestorhabitos/saltar-la-cuerda.png', nombre: 'Saltar la cuerda', link: 'movimiento4.html' }
+        { img: '/img/gestorhabitos/estiramiento.png', nombre: 'Estiramientos matutinos', link: '/estiramientos' },
+        { img: '/img/gestorhabitos/correr.png', nombre: 'Correr', link: '/Correr' },
+        { img: '/img/gestorhabitos/bici.png', nombre: 'Andar en bicicleta', link: '/AndarEnBicicleta' },
+        { img: '/img/gestorhabitos/saltar-la-cuerda.png', nombre: 'Saltar la cuerda', link: '/SaltarLaCuerda' }
     ],
     bienestar: [
-        { img: '../img/gestorhabitos/respiro.png', nombre: 'Respiración consciente', link: '/bienestar1' },
-        { img: '../img/gestorhabitos/baño.png', nombre: 'Baño relajante', link: '/bienestar2' }
-        // Agrega más si es necesario
+        { img: '/img/gestorhabitos/dormir.png', nombre: 'Horas de sueño', link: '/sueño' },
+        { img: '/img/gestorhabitos/sintelefono.png', nombre: 'Desintoxicación Digital', link: '/sueño' },
+        { img: '/img/gestorhabitos/piel.png', nombre: 'Cuidado de la Piel', link: '/sueño' },
+        { img: '/img/gestorhabitos/alimentacion.png', nombre: 'Alimentación', link: '/sueño' },
     ],
     mental: [
-        { img: '../img/gestorhabitos/lectura.png', nombre: 'Leer', link: '/mental1' },
-        { img: '../img/gestorhabitos/meditacion.png', nombre: 'Meditar', link: '/mental2' }
-        // Agrega más si es necesario
+        { img: '/img/gestorhabitos/ordenar.png', nombre: 'Ordenar Espacio Personal', link: '/ordenarEspacio' },
+        { img: '/img/gestorhabitos/leer.png', nombre: 'Lectura', link: '/Lectura' },
+        { img: '/img/gestorhabitos/meditacion.png', nombre: 'Meditación', link: '/Meditacion' },
+        { img: '/img/gestorhabitos/escuchar-musica.png', nombre: 'Escuchar Musica Relajante', link: '/EscucharMusicaRelajante' },
     ]
 };
 
