085bfca35fc4340947b003aa4b0a294c5afe135d
DiegoRE-15 <diegoespinosar15@gmail.com>
2025-05-20 23:17:38 -0600

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, logout, recoverPassword, validateResetToken, resetPassword, deleteAccount, updateProfile, saveMood  } = require('../controllers/authController');
+const { register, login, logout, recoverPassword, validateResetCode, resetPassword, deleteAccount, updateProfile, saveMood  } = require('../controllers/authController');
 const { upload } = require('../utils/cloudinary');
 const { authMiddleware } = require('../middlewares/authMiddleware'); 
 const { validateRegister, validateLogin, validateDeleteAcc } = require('../middlewares/validateAuth');
@@ -10,7 +10,7 @@ router.post('/login', validateLogin, login);
 router.post('/deleteAcc', validateDeleteAcc, deleteAccount);
 router.get('/logout', logout);
 router.post('/recover-password', recoverPassword);
-router.post('/validate-reset-token', validateResetToken);
+router.post('/validate-code', validateResetCode);
 router.post('/reset-password', resetPassword);
 router.post('/mood', authMiddleware, saveMood);
 router.put('/user/profile', authMiddleware, upload.single('profilePic'), updateProfile);
